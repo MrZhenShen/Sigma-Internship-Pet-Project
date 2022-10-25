1b11bedd8aaa23230af54b0855ed8b40efe94506
mrzhenshen <mrzhenshen@gmail.com>
2022-10-25 15:01:33 +0300

    init

---
+++ src/main/java/sigma/internship/PetProject/PetProjectApplication.java
@@ -0,0 +1,13 @@
+package sigma.internship.PetProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sigma/internship/PetProject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sigma.internship.PetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
