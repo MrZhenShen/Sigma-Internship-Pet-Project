a9717087c52d03f3c4f54fff041ff8d48f370ac9
mrzhenshen <mrzhenshen@gmail.com>
2022-11-14 17:49:38 +0200

    add logs

--- src/main/java/sigma/internship/petProject/service/GameServiceImpl.java
+++ src/main/java/sigma/internship/petProject/service/GameServiceImpl.java
@@ -1,6 +1,7 @@
 package sigma.internship.petProject.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import sigma.internship.petProject.dto.GameDto;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GameServiceImpl implements GameService {
 
     private final GameRepository gameRepository;
@@ -21,35 +23,45 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public List<GameShortDto> findAllPreviews() {
+        log.info("Started retrieving all game objects");
         List<Game> gameList = gameRepository.findAll();
         if (gameList.size() > 0) {
+            log.info("{} game objects were retrieved from database", gameList.size());
             return gameList
                     .stream()
                     .map(gameMapper::toShortDto)
                     .toList();
         } else {
+            log.error("Retrieving all game data: {}", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
             throw new WebException(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         }
     }
 
     @Override
     public List<GameDto> findAll() {
+        log.info("Started retrieving all game objects");
         List<Game> gameList = gameRepository.findAll();
         if (gameList.size() > 0) {
+            log.info("{} game objects were retrieved from database", gameList.size());
             return gameList
                     .stream()
                     .map(gameMapper::toDto)
                     .toList();
         } else {
+            log.error("Retrieving all game data: {}", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
             throw new WebException(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         }
     }
 
     @Override
     public GameShortDto findById(long id) {
+        log.info("Started retrieving game object with id: {}", id);
         return gameRepository
                 .findById(id)
                 .map(gameMapper::toShortDto)
-                .orElseThrow(() -> new WebException(HttpStatus.NOT_FOUND, "Game not found"));
+                .orElseThrow(() -> {
+                    log.error("Game object with id: {} was not found", id);
+                    throw new WebException(HttpStatus.NOT_FOUND, "Game not found");
+                });
     }
 }
--- src/main/java/sigma/internship/petProject/service/UserDetailsServiceImpl.java
+++ src/main/java/sigma/internship/petProject/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package sigma.internship.petProject.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,14 +14,20 @@ import java.util.Collections;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Started retrieving user data with username: {}", username);
         User user = userRepository
                 .findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User details not found for the user " + username));
+                .orElseThrow(() -> {
+                    log.error("Error with retrieving user with username: {}", username);
+                    throw new UsernameNotFoundException("User details not found for the user " + username);
+                });
+        log.info("User with username: {} was successfully retrieved", username);
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
