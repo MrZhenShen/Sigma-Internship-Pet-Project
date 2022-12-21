7431d84102453a64047607a5c71b53aaa69ac83d
mrzhenshen <mrzhenshen@gmail.com>
2022-12-21 11:23:46 +0200

    fix issue with wildcard imports

--- src/main/java/sigma/internship/petProject/controller/GameSessionController.java
+++ src/main/java/sigma/internship/petProject/controller/GameSessionController.java
@@ -10,7 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import sigma.internship.petProject.dto.GameSessionDto;
 import sigma.internship.petProject.service.GameSessionService;
 
--- src/main/java/sigma/internship/petProject/controller/MoneyBalanceController.java
+++ src/main/java/sigma/internship/petProject/controller/MoneyBalanceController.java
@@ -7,7 +7,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import sigma.internship.petProject.dto.MoneyBalanceDto;
 import sigma.internship.petProject.service.MoneyBalanceService;
 
